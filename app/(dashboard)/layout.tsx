9f56f2b07d32078fabc51a28b034f59b8b0bea9a
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-03-20 16:07:14 +0100

    chore: created the different pages fro the app

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../globals.css";
 import { Inter, Nunito } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 const nunito = Nunito({ subsets: ["latin"] });
---
+++ app/(dashboard)/trade/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TradePage() {
+  return (
+    <main className="h-screen flex flex-col w-full items-center justify-center">
+      <button className="px-4 py-3 bg-teal-800 text-white rounded-md focus:ring-2">
+        Make Api Call
+      </button>
+    </main>
+  );
+}
