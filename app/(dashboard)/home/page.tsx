8c0101a87901c30654f3acb2747a7dbd7d026ee0
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-03-26 23:34:56 +0100

    feat: layed out the tables on the dashboard

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -2,12 +2,14 @@ import ApySection from "@/components/apy";
 import Banner from "@/components/banner";
 import Example from "@/components/omo";
 import StatCard, { stats } from "@/components/stat-card";
-import Table from "@/components/table";
+import SupplyTable from "@/components/table";
+import BorrowTable from "@/components/table/borrow";
+import SuppliedTable from "@/components/table/supplied";
 import React from "react";
 
 export default function Dashboard() {
   return (
-    <main className="space-y-10">
+    <main className="space-y-10 pb-20">
       <Banner />
       {/* <Example /> */}
       <ApySection />
@@ -16,7 +18,28 @@ export default function Dashboard() {
           <StatCard key={stat.text} {...stat} />
         ))}
       </div>
-      <Table />
+      <div className="grid grid-col-1 md:grid-cols-2 gap-5">
+        <div className="space-y-6">
+          <div className="space-y-2">
+            <h3 className="uppercase font-bold text-sm text-gray-800">
+              Supplied
+            </h3>
+            <SuppliedTable />
+          </div>
+          <div className="space-y-2">
+            <h3 className="uppercase font-bold text-sm text-gray-800">
+              Supply MARKET
+            </h3>
+            <SupplyTable />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <h3 className="uppercase font-bold text-sm text-gray-800">
+            borrow MARKET
+          </h3>
+          <BorrowTable />
+        </div>
+      </div>
     </main>
   );
 }
--- components/table/supplied.tsx
+++ components/table/supplied.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ChevronUpDownIcon } from "@heroicons/react/24/outline";
 
-function BorrowTable() {
+function SuppliedTable() {
   return (
-    <div className="shadow-sm overflow-hidden p-4 bg-white mb-8 rounded">
+    <div className="shadow-sm overflow-hidden p-3 bg-white mb-8 rounded">
       <table className="border-collapse table-auto  w-full text-sm rounded-full border-spacing-0 gap-5">
         <thead>
           <tr className="bg-[#FAFAFC] py-2">
-            <th className="font-bold p-4 pl-8 rounded-tl-lg rounded-bl-lg text-slate-500 text-left">
+            <th className="font-bold p-4 pl-8 rounded-tl-lg rounded-bl-lg text-slate-700 text-left">
               <div className="flex items-end gap-1">
                 Asset
                 <ChevronUpDownIcon
@@ -16,7 +16,7 @@ function BorrowTable() {
                 />
               </div>
             </th>
-            <th className="font-bold p-4  text-slate-500  text-left">
+            <th className="font-bold p-4 text-slate-700  text-left">
               <div className="flex items-end gap-1">
                 APY
                 <ChevronUpDownIcon
@@ -25,7 +25,7 @@ function BorrowTable() {
                 />
               </div>
             </th>
-            <th className="font-bold p-4 pr-8  text-slate-500 text-left rounded-tr-lg rounded-br-lg">
+            <th className="font-bold p-4 text-slate-700 text-left rounded-tr-lg rounded-br-lg">
               <div className="flex items-end gap-1">
                 Wallet
                 <ChevronUpDownIcon
@@ -34,7 +34,7 @@ function BorrowTable() {
                 />
               </div>
             </th>
-            <th className="font-bold p-4 pr-8  text-slate-500 text-left rounded-tr-lg rounded-br-lg">
+            <th className="font-bold p-4 text-slate-700 text-left rounded-tr-lg rounded-br-lg">
               <div className="flex items-end gap-1">
                 Colleteral
                 <ChevronUpDownIcon
@@ -43,7 +43,7 @@ function BorrowTable() {
                 />
               </div>
             </th>
-            <th className="font-bold p-4 pr-8  text-slate-500 text-left rounded-tr-lg rounded-br-lg">
+            <th className="font-bold p-4 pr-8 text-slate-700 text-left rounded-tr-lg rounded-br-lg">
               <div className="flex items-end gap-1">
                 Action
                 <ChevronUpDownIcon
@@ -56,27 +56,11 @@ function BorrowTable() {
         </thead>
         <tbody className="bg-white">
           <tr>
-            <td className="p-4 pl-8 text-slate-500">
-              The Sliding Mr. Bones (Next Stop, Pottersville)
-            </td>
-            <td className="p-4 text-slate-500">Malcolm Lockyer</td>
-            <td className="p-4 text-slate-500">1961</td>
-            <td className="p-4 text-slate-500">1961</td>
-            <td className="p-4 pr-8 text-slate-500">1961</td>
-          </tr>
-          <tr>
-            <td className="p-4 pl-8 text-slate-500">Witchy Woman</td>
-            <td className="p-4 text-slate-500">The Eagles</td>
-            <td className="p-4 text-slate-500">1972</td>
-            <td className="p-4 text-slate-500">1961</td>
-            <td className="p-4 pr-8 text-slate-500">1972</td>
-          </tr>
-          <tr>
-            <td className="p-4 pl-8 text-slate-500">Shining Star</td>
-            <td className="p-4 text-slate-500">Earth, Wind, and Fire</td>
-            <td className="p-4 text-slate-500">1972</td>
-            <td className="p-4 text-slate-500">1975</td>
-            <td className="p-4 pr-8 text-slate-500">1975</td>
+            <td className="p-4 pl-8 text-slate-700">TRX</td>
+            <td className="p-4 text-slate-700">4.65%</td>
+            <td className="p-4 text-slate-700">1961</td>
+            <td className="p-4 text-slate-700">1961</td>
+            <td className="p-4 pr-8 text-slate-700">1961</td>
           </tr>
         </tbody>
       </table>
@@ -84,4 +68,4 @@ function BorrowTable() {
   );
 }
 
-export default BorrowTable;
+export default SuppliedTable;
